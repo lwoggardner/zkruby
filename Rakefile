dade2fde10e27cf982c9684052bff2bd90405a9b
Grant Gardner <grant@lastweekend.com.au>
2011-12-30 00:32:33 +1100

    make jute compilation work with hoe 2.12.5+

--- Rakefile
+++ Rakefile
@@ -1,14 +1,14 @@
 # -*- ruby -*-
+# hoe  2.12.5 goes looking for the plugins so we have to do it this way..
+$LOAD_PATH.unshift File.dirname(__FILE__) + '/jute/lib'
 
 require 'rubygems'
 require 'hoe'
 require './yard_ext/enum_handler'
-require './jute/lib/jute.rb'
-require './jute/lib/hoe.rb'
 
 
 # Hoe.plugin :compiler
-# Hoe.plugin :gem_prelude_sucks
+#Hoe.plugin :gem_prelude_sucks
 Hoe.plugin :git
 # Hoe.plugin :inline
 # Hoe.plugin :racc
@@ -20,10 +20,9 @@ Hoe.spec 'zkruby' do
   self.readme_file="README.rdoc"
   developer('Grant Gardner', 'grant@lastweekend.com.au')
   extra_deps << [ 'slf4r' , '>= 0.4.2' ]
-  
+
   self.jute_modules = {
       "org.apache.zookeeper.data" => "ZooKeeper::Data",
       "org.apache.zookeeper.proto" => "ZooKeeper::Proto"}
 end
-
 # vim: syntax=ruby
--- jute/lib/hoe/jute.rb
+++ jute/lib/hoe/jute.rb
@@ -1,39 +1,43 @@
 # coding: utf-8
+require 'jute'
 
 module Hoe::Jute
+   attr_accessor :jute
    attr_accessor :jute_tasks
    attr_accessor :jute_modules
+
    #attr_accessor :jute_compiler
    def initialize_jute
-        jute_tasks = [:test,:spec]
+        self.jute_tasks = [:test,:spec,:package]
         dependency 'citrus', '~> 2.4.0', :development
         #dependency 'jute' # if jute is ever a separate gem
         dependency 'bindata', '~> 1.4.1'
    end
 
    def define_jute_tasks
-         jute_compiler = Jute::Compiler.new()
+         jute_compiler = ::Jute::Compiler.new()
          jute_files   = self.spec.files.find_all { |f| f =~ /\.jute$/ }
 
          record_files = jute_files.map { |f| f.pathmap("%{src,lib}X.rb") }
-
          self.clean_globs += record_files
+
 
           rule ".rb" => ["%{lib,src}X.jute"] do |t|
               File.open(t.source) do |input|
                 File.open(t.name,"w") do |output|
+                    puts "compiling #{input.inspect} to #{output.inspect}"
                     jute_compiler.compile(input,output,jute_modules)
                 end
               end
           end
 
           desc "generate jute records" unless jute_files.empty?
-          task :compile_jute
+          task :jute
 
-          task :compile_jute => record_files
+          task :jute => record_files
 
           jute_tasks.each do |t|
-            task t => [:compile_jute]
+            task t => [:jute]
           end 
    end
 
